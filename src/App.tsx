7ac07dd473d18d61299784967637608eabf2d992
Александр Демещенко <hell-llex@Aleksandrs-Air.mshome.net>
2023-03-19 16:19:49 +0300

    feat: add base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 import './App.css';
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import { HomePage } from './pages/HomePage';
+import { AboutPage } from './pages/AboutPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+    <>
+      <header className="header">
+        <nav className="nav">
+          <Link to="/" className="nav-item">
+            Home
+          </Link>
 
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+          <Link to="/about" className="nav-item">
+            About us
+          </Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export { HomePage };
