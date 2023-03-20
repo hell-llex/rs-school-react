dac8837c01304e4d7b78eddbbb455bfd9aa1d8e6
Александр Демещенко <hell-llex@Aleksandrs-Air.mshome.net>
2023-03-20 17:37:09 +0300

    fix: fix search and cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,28 +1,38 @@
 import './Cards.css';
 import React from 'react';
-import films from '../films.json';
+import filmsData from '../films.json';
+import { FilmsProps } from '../types/type';
+
+export class FilmCards extends React.Component<FilmsProps> {
+  render() {
+    const film = this.props.film;
+    return (
+      <div className="card">
+        <div
+          className="image"
+          style={{
+            position: 'absolute',
+            background: `no-repeat center/100% url("${film.posterUrlPreview}")`,
+            height: '100%',
+            width: '100%',
+          }}
+        ></div>
+        <div className="container-info">
+          <div className="titleRu">{film.nameRu}</div>
+          <div className="titleEn">{film.nameEn}</div>
+          <div className="year">{film.year}</div>
+          <div className="rating">{film.rating}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export function Cards() {
   return (
     <div className="container-cards">
-      {films.map((film) => (
-        <div className="card" key={film.filmId}>
-          <div
-            className="image"
-            style={{
-              position: 'absolute',
-              background: `no-repeat center/100% url("${film.posterUrlPreview}")`,
-              height: '100%',
-              width: '100%',
-            }}
-          ></div>
-          <div className="container-info">
-            <div className="titleRu">{film.nameRu}</div>
-            <div className="titleEn">{film.nameEn}</div>
-            <div className="year">{film.year}</div>
-            <div className="rating">{film.rating}</div>
-          </div>
-        </div>
+      {filmsData.map((film) => (
+        <FilmCards key={film.filmId} film={film} />
       ))}
     </div>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,44 @@
+import { SearchInfo } from '../types/type';
 import './Search.css';
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
-export function Search() {
-  return (
-    <div className="container-search">
-      <input type="search" name="" id="" placeholder="Search" className="search" />
-      <button type="submit" className="btn-search">
-        Search
-      </button>
-    </div>
-  );
+export class Search extends React.Component<Record<string, never>, SearchInfo> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = { searchText: '' };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="container-search">
+        <input
+          defaultValue={this.state.searchText}
+          onChange={this.handleInput}
+          type="search"
+          placeholder="Search"
+          className="search"
+        />
+        <button type="submit" className="btn-search">
+          Search
+        </button>
+      </div>
+    );
+  }
+
+  handleInput(e: ChangeEvent<HTMLInputElement>) {
+    this.setState({ searchText: e.target.value });
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('searchText'))
+      this.setState({
+        searchText: JSON.parse(JSON.stringify(localStorage.getItem('searchText'))),
+      });
+    localStorage.setItem('searchText', this.state.searchText);
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('searchText', this.state.searchText);
+  }
 }
