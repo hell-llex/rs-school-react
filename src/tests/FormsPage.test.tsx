e652364cca7ae1a266c5baa376729a0062d5ad2e
Александр Демещенко <hell-llex@Aleksandrs-MacBook-Air.local>
2023-03-29 20:36:32 +0300

    fix: delete comment

--- src/tests/FormsPage.test.tsx
+++ src/tests/FormsPage.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FormsPage } from '../pages/FormsPage';
 import { test, expect } from 'vitest';
-import { render, fireEvent, getByRole, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('FormsPage', () => {
@@ -44,32 +44,4 @@ describe('FormsPage', () => {
 
     fireEvent.click(getByText('Submit'));
   });
-
-  // test('displays an error message when required fields are empty', () => {
-  //   const { getByLabelText, getByText } = render(<FormsPage />);
-  //   fireEvent.click(getByText('Submit'));
-  //   expect(getByText('Required field.')).toBeInTheDocument();
-  //   fireEvent.change(getByLabelText('Author'), { target: { value: 'John Doe' } });
-  //   fireEvent.click(getByText('Submit'));
-  //   expect(getByText('Required field.')).toBeInTheDocument();
-  //   fireEvent.change(getByLabelText('Description'), { target: { value: 'A beautiful photo' } });
-  //   fireEvent.click(getByText('Submit'));
-  //   expect(getByText('Required field.')).toBeInTheDocument();
-  //   fireEvent.change(getByLabelText('Date'), { target: { value: '2022-03-29' } });
-  //   fireEvent.click(getByText('Submit'));
-  //   expect(getByText('Required field.')).toBeInTheDocument();
-  //   fireEvent.change(getByLabelText('Image'), { target: { files: [new File([], 'test.png')] } });
-  //   fireEvent.click(getByText('Submit'));
-  //   expect(getByText('Required field.')).not.toBeInTheDocument();
-  // });
-
-  // test('displays an error message when input values are invalid', () => {
-  //   const { getByLabelText, getByText } = render(<FormsPage />);
-  //   fireEvent.change(getByLabelText('Author'), { target: { value: 'J' } });
-  //   fireEvent.change(getByLabelText('Description'), { target: { value: 'A' } });
-  //   fireEvent.change(getByLabelText('Date'), { target: { value: '2024-03-29' } });
-  //   fireEvent.change(getByLabelText('Category'), { target: { value: '' } });
-  //   fireEvent.click(getByText('Submit'));
-  //   expect(getByText('Invalid input.')).toBeInTheDocument();
-  // });
 });
