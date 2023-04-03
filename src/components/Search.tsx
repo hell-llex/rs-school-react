10fbc88dcf043a3070c6599dd8d37bca2799ef54
Александр Демещенко <hell-llex@Aleksandrs-MacBook-Air.local>
2023-04-03 17:30:13 +0300

    feat: change search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,43 +1,34 @@
 import '../style/Search.css';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState, useEffect } from 'react';
 
-export class Search extends React.Component {
-  state = { searchText: '' };
-  constructor(props: never) {
-    super(props);
-    this.handleInput = this.handleInput.bind(this);
+export function Search() {
+  const [searchText, setSearchText] = useState(
+    localStorage.getItem('searchText')
+      ? JSON.parse(JSON.stringify(localStorage.getItem('searchText')))
+      : ''
+  );
+
+  function handleInput(e: ChangeEvent<HTMLInputElement>) {
+    setSearchText(e.target.value);
   }
 
-  render() {
-    return (
-      <div className="container-search">
-        <input
-          defaultValue={this.state.searchText}
-          onChange={this.handleInput}
-          type="text"
-          placeholder="Search"
-          className="search"
-          role="search"
-        />
-        <button type="submit" className="btn-search">
-          Search
-        </button>
-      </div>
-    );
-  }
+  useEffect(() => {
+    localStorage.setItem('searchText', searchText);
+  }, [searchText]);
 
-  handleInput(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({ searchText: e.target.value });
-  }
-
-  componentDidMount() {
-    if (localStorage.getItem('searchText'))
-      this.setState({
-        searchText: JSON.parse(JSON.stringify(localStorage.getItem('searchText'))),
-      });
-  }
-
-  componentWillUnmount() {
-    localStorage.setItem('searchText', this.state.searchText);
-  }
+  return (
+    <div className="container-search">
+      <input
+        defaultValue={searchText}
+        onChange={handleInput}
+        type="text"
+        placeholder="Search"
+        className="search"
+        role="search"
+      />
+      <button type="submit" className="btn-search">
+        Search
+      </button>
+    </div>
+  );
 }
